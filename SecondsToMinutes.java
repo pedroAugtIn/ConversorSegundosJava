c244c0536fff70e9f623b50aae380427bcfb297d
Pedro Augusto Inácio <158518938+pedroAugtIn@users.noreply.github.com>
2024-06-13 17:28:03 -0300

    Update SecondsToMinutes.java

--- SecondsToMinutes.java
+++ SecondsToMinutes.java
@@ -2,18 +2,19 @@ public class SecondsToMinutes {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(1293232));
+        System.out.println(getDurationString(-13232));
+        System.out.println(getDurationString(3599));
+        System.out.println(getDurationString(37));
     }
 
     public static String getDurationString(int seconds) {
         String formatSeconds = "";
         if (seconds < 0) {
-            return formatSeconds = "Invalid Time.";
+            return formatSeconds = "Invalid Time. (" + seconds + ") must be a positive integer value";
         } else if (seconds >= 0 && seconds < 60) {
-            formatSeconds = "00h 00m " + seconds + "s";
-            return formatSeconds;
+            return formatSeconds = "00h 00m " + seconds + "s";
         } else if (seconds >= 60 && seconds < 3600) {
-            formatSeconds = "00h " + seconds / 60 + "m " + seconds % 60 + "s";
-            return formatSeconds;
+            return formatSeconds = "00h " + seconds / 60 + "m " + seconds % 60 + "s";
         } else if (seconds >= 3600) {
             int hours = seconds / 3600;
             int minutes = seconds % 3600 / 60;
